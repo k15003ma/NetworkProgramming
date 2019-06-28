bba92b740769cc248e08017148715b33ceafba3c
k15003ma <k15003ma@apps.kct.ac.jp>
2019-06-28 10:35:40 +0900

    20190628

--- www/server.c
+++ www/server.c
@@ -14,6 +14,26 @@ void DieWithError(char*errorMessage){
 void commun(int sock){
     char buf[BUF_SIZE];
 	int len_r;
+	char response[BUF_SIZE];
+	
+	while((len_r = recv(sock,buf,BUF_SIZE,0) > 0){
+		buf[len_r] = '\0';
+		printf("%s\n".buf);
+		if(strstr(buf,"\r\n\r\n")){
+			break;
+		}		
+	}
+	
+	if(len_r <= 0)
+	  DieWithError("received() failed.");
+		
+	sprintf(response,"HTTP/1.1 200 OK\r\n");
+		if(send(sock,response,strlen(response),0) != strlen(buf));
+		   DieWithError("send() sent a message of unexpected bytes");
+}
+	
+	printf("received HTTP Request.\n");
+	
 	if((len_r = recv(sock,buf,BUF_SIZE,0))<=0)
 	   DieWithError("recv()failed");
 	buf[len_r] = '\0';
@@ -33,7 +53,7 @@ int main(int argc,char**argv){
 	servAddress.sin_addr.s_addr = htonl(INADDR_ANY);
 	servAddress.sin_port = htons(80);
 	bind(servSock,(struct sockaddr*)&servAddress,sizeof(servAddress));
-	//
+	
 	listen(servSock,5);
 	while(1){
 		szClientAddr = sizeof(clientAddress);
